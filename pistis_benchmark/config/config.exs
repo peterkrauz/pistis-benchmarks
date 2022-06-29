cb133ee39c45e771f6cbe695f388b7e7f4f5dd49
Peter Krause <krausepeterm@gmail.com>
2022-06-29 13:27:00 -0300

    chore: Experiment stuff

--- pistis_benchmark/config/config.exs
+++ pistis_benchmark/config/config.exs
@@ -5,9 +5,13 @@ config :pistis_benchmark, shutdown_in: 300000
 
 config :pistis, machine: PistisBenchmark.KVStore
 config :pistis, known_hosts: [
-  :"pistis_node_1@ip",
-  :"pistis_node_2@ip",
-  :"pistis_node_3@ip",
-  :"pistis_node_4@ip",
-  :"pistis_node_5@ip",
+  :"pistis_node_1@10.10.1.3",
+  :"pistis_node_2@10.10.1.4",
+  :"pistis_node_3@10.10.1.5",
+  :"pistis_node_4@10.10.1.6",
+  :"pistis_node_5@10.10.1.7",
+  :"pistis_node_6@10.10.1.8",
+  :"pistis_node_7@10.10.1.9",
+  :"pistis_node_8@10.10.1.10",
+  :"pistis_node_9@10.10.1.11",
 ]
