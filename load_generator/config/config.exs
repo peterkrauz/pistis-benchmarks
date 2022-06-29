0ade81e9799a209dc6709d3e6e65b199743da7c3
Peter Krause <krausepeterm@gmail.com>
2022-06-29 09:53:18 -0300

    chore: Experiment stuff

--- load_generator/config/config.exs
+++ load_generator/config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :load_generator, target_host: "localhost"
-config :load_generator, target_port: 5456
+config :load_generator, target_port: 5455
 config :load_generator, worker_count: 5
-config :load_generator, heartbeat: 1
-config :load_generator, shutdown_in: 180000
+config :load_generator, heartbeat: 10
+config :load_generator, shutdown_in: 300000
